451a0003c660eb5c5efb92e70ade609ef907438e
40582229 <40582229@live.napier.ac.uk>
2025-06-22 20:00:27 +0100

    feature/added-env-cosntants

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -5,29 +5,29 @@ import TileLayer from 'ol/layer/Tile';
 import { View, Geolocation } from 'ol';
 import { OSM } from 'ol/source';
 import UsersLocationLayer from '../../layers/PositionLayer/UsersLocationLayer';
+import Control from 'ol/control/Control';
 
-const deviceId = Math.random().toString(36).substring(2, 15);
+let deviceId = '0';
+const newDevcId =localStorage.getItem('dId')
+if(newDevcId){
+  deviceId = newDevcId;
+}else{
+  deviceId = Math.random().toString(36).substring(2, 15);
+  localStorage.setItem('dId', deviceId);
+}
 
 const rasterLayer = new TileLayer({
   source: new OSM(),
 });
-
-let ws = new WebSocket('wss://cold-ways-live.loca.lt');
+const websocketApiUrl = process.env.VITE_APP_WEBSOCET_CONNECTION_URL || '';
+let ws = new WebSocket(websocketApiUrl);
 ws.binaryType = 'arraybuffer';
 const MapView = () => {
-  const [position, setPosition] = useState([0,0]);
+  const positionRef = useRef([0,0]);
   const userLocationLayer = useRef<UsersLocationLayer>(new UsersLocationLayer());
-  const viewRef = useRef<View>(new View({zoom:10, center:[...position]}));
+  const viewRef = useRef<View>(new View({zoom:10, center:positionRef.current}));
   const mapRef = useRef<HTMLDivElement>(null);
   
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setPosition([position.coords.latitude, position.coords.longitude]);
-    });
-    ws.onopen = () => {
-      console.log('WebSocket connected');
-    };
-  }, []);
 
   useEffect(() => {
     ws.onopen = () => {
@@ -35,21 +35,29 @@ const MapView = () => {
     };
   }, []);
 
-  useEffect(() => {
-
-    viewRef.current.setCenter([...position]);
-    
-    
-  }, [position]);
 
   useEffect(() => {
     if(!viewRef.current || !userLocationLayer) return;
 
-    const map = new Map({
-      target: mapRef.current!,
-      layers: [rasterLayer, userLocationLayer.current.getUsersLocationLayer()],
-      view:viewRef.current,
-    });
+    const btn = (getPos: () => number[])=>{
+      const button = document.createElement('button');
+      button.innerHTML = '📍';
+      button.className = 'ol-custom-button';
+      button.title = 'Center Map';
+
+      button.addEventListener('click', (e) => {
+        e.stopPropagation(); // Prevent map interactions
+        e.preventDefault();
+        viewRef.current.setCenter([...getPos()]); // Center map
+      });
+
+      const element = document.createElement('div');
+      element.className = 'ol-unselectable ol-control';
+      element.appendChild(button);
+
+      return new Control({ element });
+  }
+
     const geolocation = new Geolocation({
       tracking: true,
       trackingOptions: {
@@ -58,13 +66,22 @@ const MapView = () => {
       projection: viewRef.current.getProjection(),
     });
 
+    const map = new Map({
+      controls:[btn(()=>positionRef.current)],
+      target: mapRef.current!,
+      layers: [rasterLayer, userLocationLayer.current.getUsersLocationLayer()],
+      view:viewRef.current,
+    });
+
+   
+
     geolocation.on('change:position', () => {
       const pos = geolocation.getPosition();
 
       if (pos && userLocationLayer) {
         const userObject = { deviceId, pos, color: userLocationLayer.current.imageUrl()};
         userLocationLayer.current.setUserLocation(deviceId, pos, userLocationLayer.current.imageUrl());
-        setPosition(pos);
+        positionRef.current = pos;
         
         if (ws.readyState === ws.OPEN) {
           ws.send(JSON.stringify(userObject));
@@ -80,6 +97,7 @@ const MapView = () => {
         userLocationLayer.current.setUserLocation(cords.deviceId, cords.pos, cords.color);
       }
     });
+
 
     return () => {
       map.setTarget(undefined); // cleanup on unmount
