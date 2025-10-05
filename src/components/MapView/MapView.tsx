43b277cd38f6505bd1ee073fcf3593f49486bf71
40582229 <40582229@live.napier.ac.uk>
2025-10-05 20:36:23 +0100

    added line drawing

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -2,32 +2,53 @@ import { useEffect, useRef, useState } from 'react';
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import TileLayer from 'ol/layer/Tile';
-import { View, Geolocation } from 'ol';
+import { View, Geolocation, Feature } from 'ol';
 import { OSM } from 'ol/source';
 import UsersLocationLayer from '../../layers/PositionLayer/UsersLocationLayer';
 import Control from 'ol/control/Control';
+import { LineString } from 'ol/geom';
+import Style from 'ol/style/Style';
+import Stroke from 'ol/style/Stroke';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import XYZ from "ol/source/XYZ";
 
 let deviceId = '0';
-const newDevcId =localStorage.getItem('dId')
-if(newDevcId){
+const newDevcId = localStorage.getItem('dId');
+if (newDevcId) {
   deviceId = newDevcId;
-}else{
+} else {
   deviceId = Math.random().toString(36).substring(2, 15);
   localStorage.setItem('dId', deviceId);
 }
-
+const hillshade = new TileLayer({
+  source: new XYZ({
+    url: "https://tile.opentopomap.org/{z}/{x}/{y}.png"
+  }),
+  opacity: 1
+});
+const satShade = new TileLayer({
+  source: new XYZ({
+    url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+  }),
+  opacity: 0.5
+});
 const rasterLayer = new TileLayer({
   source: new OSM(),
 });
-const websocketApiUrl = process.env.VITE_APP_WEBSOCET_CONNECTION_URL || '';
+const websocketApiUrl = import.meta.env.VITE_APP_WEBSOCET_CONNECTION_URL || '';
 let ws = new WebSocket(websocketApiUrl);
 ws.binaryType = 'arraybuffer';
 const MapView = () => {
-  const positionRef = useRef([0,0]);
-  const userLocationLayer = useRef<UsersLocationLayer>(new UsersLocationLayer());
-  const viewRef = useRef<View>(new View({zoom:10, center:positionRef.current}));
+  const positionRef = useRef([0, 0]);
+  const userLocationLayer = useRef<UsersLocationLayer>(
+    new UsersLocationLayer(),
+  );
+  const viewRef = useRef<View>(
+    new View({ zoom: 10, center: positionRef.current}),
+  );
   const mapRef = useRef<HTMLDivElement>(null);
-  
+
 
   useEffect(() => {
     ws.onopen = () => {
@@ -35,11 +56,19 @@ const MapView = () => {
     };
   }, []);
 
+  window.onerror = (msg, src, line, col, err) => {
+    const div = document.createElement('div');
+    div.style =
+      'position:fixed;bottom:0;left:0;width:100%;background:red;color:white;padding:8px;font-family:monospace;z-index:9999;';
+    div.textContent = `Error: ${msg} @ ${src}:${line}:${col}`;
+    document.body.appendChild(div);
+    return false;
+  };
 
   useEffect(() => {
-    if(!viewRef.current || !userLocationLayer) return;
+    if (!viewRef.current || !userLocationLayer) return;
 
-    const btn = (getPos: () => number[])=>{
+    const btn = (getPos: () => number[]) => {
       const button = document.createElement('button');
       button.innerHTML = '📍';
       button.className = 'ol-custom-button';
@@ -56,7 +85,8 @@ const MapView = () => {
       element.appendChild(button);
 
       return new Control({ element });
-  }
+    };
+
 
     const geolocation = new Geolocation({
       tracking: true,
@@ -67,22 +97,32 @@ const MapView = () => {
     });
 
     const map = new Map({
-      controls:[btn(()=>positionRef.current)],
+      controls: [btn(() => positionRef.current)],
       target: mapRef.current!,
-      layers: [rasterLayer, userLocationLayer.current.getUsersLocationLayer()],
-      view:viewRef.current,
+      layers: [
+        hillshade,
+        userLocationLayer.current.getUsersLocationLayer(),
+      ],
+      view: viewRef.current,
     });
-
-   
 
     geolocation.on('change:position', () => {
       const pos = geolocation.getPosition();
 
       if (pos && userLocationLayer) {
-        const userObject = { deviceId, pos, color: userLocationLayer.current.imageUrl()};
-        userLocationLayer.current.setUserLocation(deviceId, pos, userLocationLayer.current.imageUrl());
+        const userObject = {
+          deviceId,
+          pos,
+          color: userLocationLayer.current.imageUrl(),
+        };
+        userLocationLayer.current.setUserLocation(
+          deviceId,
+          pos,
+          userLocationLayer.current.imageUrl(),
+        );
+
         positionRef.current = pos;
-        
+
         if (ws.readyState === ws.OPEN) {
           ws.send(JSON.stringify(userObject));
         }
@@ -91,18 +131,23 @@ const MapView = () => {
     });
 
     ws.addEventListener('message', (msg) => {
-      const text = new TextDecoder('utf-8').decode(msg.data);
-      let cords = JSON.parse(text);
-      if (cords && userLocationLayer) {
-        userLocationLayer.current.setUserLocation(cords.deviceId, cords.pos, cords.color);
-      }
-    });
 
+        const text = new TextDecoder('utf-8').decode(msg.data);
+        let cords = JSON.parse(text);
+        console.log(cords)
+        if (cords && userLocationLayer) {
+          userLocationLayer.current.setUserLocation(
+            cords.deviceId,
+            cords.pos,
+            cords.color,
+          );
+        }
+      
+    });
 
     return () => {
       map.setTarget(undefined); // cleanup on unmount
     };
-
   }, []);
 
   return <div ref={mapRef} style={{ width: '100%', height: '100vh' }} />;
--- src/layers/PositionLayer/UsersLocationLayer.ts
+++ src/layers/PositionLayer/UsersLocationLayer.ts
@@ -1,5 +1,5 @@
 import { Feature } from 'ol';
-import { Point } from 'ol/geom';
+import { Geometry, LineString, Point } from 'ol/geom';
 import VectorLayer from 'ol/layer/Vector';
 import { Source } from 'ol/source';
 import VectorSource from 'ol/source/Vector';
@@ -8,21 +8,27 @@ import Fill from 'ol/style/Fill';
 import Stroke from 'ol/style/Stroke';
 import Style from 'ol/style/Style';
 
-
-class UsersLocationLayer extends VectorLayer{
-  #usersLocationsSource: VectorSource;
-  #usersLocationLayer:VectorLayer;
-  constructor(){
+class UsersLocationLayer extends VectorLayer {
+  #usersLocationsSource: VectorSource<Feature<any>>;
+  #usersLocationLayer: VectorLayer;
+  constructor() {
     super();
     this.#usersLocationsSource = new VectorSource();
-    this.#usersLocationLayer = new VectorLayer({source: this.#usersLocationsSource});
-  };
-
-  imageUrl = ()=>{
-    return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0'); //thisnis for now will eba color will cahneg laters
+    this.#usersLocationLayer = new VectorLayer({
+      source: this.#usersLocationsSource,
+    });
   }
 
-  #setPonterStyle = (color:string)=>{
+  imageUrl = () => {
+    return (
+      '#' +
+      Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, '0')
+    ); //thisnis for now will eba color will cahneg laters
+  };
+
+  #setPonterStyle = (color: string) => {
     return new Style({
       image: new CircleStyle({
         radius: 7,
@@ -30,19 +36,37 @@ class UsersLocationLayer extends VectorLayer{
         stroke: new Stroke({ color: 'white', width: 2 }),
       }),
     });
-  }
+  };
+
+  #setLineStyle = () => {
+    return new Style({
+      stroke: new Stroke({
+        color: 'red',
+        width: 3,
+      }),
+    });
+  };
 
   setUserLocation = (userId: string, coords: number[], color: string) => {
     const point = new Point([...coords]);
     let feature = this.#usersLocationsSource.getFeatureById(userId);
-  
+    let lineFeature = this.#usersLocationsSource.getFeatureById(userId + 'd69');
+    if(lineFeature){
+      lineFeature.getGeometry()?.appendCoordinate(coords);
+    }else{
+      const newLineFeature = new Feature(new LineString([]));
+      newLineFeature.setStyle(this.#setLineStyle());
+      newLineFeature.setId(userId + 'd69');
+      newLineFeature.getGeometry()?.appendCoordinate(coords);
+      this.#usersLocationsSource.addFeature(newLineFeature)
+    }
     if (feature) {
       feature.setGeometry(point);
     } else {
       const newFeature = new Feature({
         geometry: point,
       });
-      newFeature.setStyle(this.#setPonterStyle(color))
+      newFeature.setStyle(this.#setPonterStyle(color));
       newFeature.setId(userId); // <-- set ID here
       this.#usersLocationsSource.addFeature(newFeature);
     }
@@ -50,7 +74,7 @@ class UsersLocationLayer extends VectorLayer{
 
   getUsersLocationLayer = () => {
     return this.#usersLocationLayer;
-  }
+  };
 }
 
-export default UsersLocationLayer
+export default UsersLocationLayer;
