b2a59c9bc90615edcccd42a02ac9ffe7606a4e40
40582229 <40582229@live.napier.ac.uk>
2026-05-01 19:28:43 +0100

    adding deployment

--- src/hooks/Socket/useSocketConnect.ts
+++ src/hooks/Socket/useSocketConnect.ts
@@ -5,7 +5,7 @@ const websocketApiUrl = import.meta.env.VITE_APP_WEBSOCET_CONNECTION_URL || '';
 
 const useSocketConnect = (updateUser: (newUser: User) => void) => {
   useEffect(() => {
-    const ws = new WebSocket('ws://192.168.0.37:8081');
+    const ws = new WebSocket('ws://80.193.148.199:8081');
     ws.binaryType = 'arraybuffer';
 
     ws.onopen = () => {
