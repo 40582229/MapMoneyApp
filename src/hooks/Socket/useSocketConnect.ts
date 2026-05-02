19010321de04acbde3470f145fa94ba62454139c
40582229 <40582229@live.napier.ac.uk>
2026-05-03 00:59:13 +0100

    added chnages

--- src/hooks/Socket/useSocketConnect.ts
+++ src/hooks/Socket/useSocketConnect.ts
@@ -5,7 +5,7 @@ const websocketApiUrl = import.meta.env.VITE_APP_WEBSOCET_CONNECTION_URL || '';
 
 const useSocketConnect = (updateUser: (newUser: User) => void) => {
   useEffect(() => {
-    const ws = new WebSocket('wss://setup-moved-block-produce.trycloudflare.com');
+    const ws = new WebSocket('wss://exports-olive-hourly-saying.trycloudflare.com');
     ws.binaryType = 'arraybuffer';
 
     ws.onopen = () => {
